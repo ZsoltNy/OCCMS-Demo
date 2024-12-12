f8acfeea94b70b12e246289463f86314fecaba28
Zsolt <zsolt.nyiri1@gmail.com>
2024-12-12 14:12:04 +0100

    OCCMS DEMO wiht some functionality

---
+++ src/Modules/MastersCollab.Core/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "MastersCollab.Core",
+    Author = "The Orchard Core Team",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "MastersCollab.Core",
+    Category = "Content Management"
+)]
---
+++ src/Themes/MastersCollab.Theme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "MastersCollab.Theme",
+    Author = "The Orchard Core Team",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "MastersCollab.Theme"
+)]
